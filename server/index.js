eba9f96f0f547500e691bfc214524bbe6959995f
Meghdad Hadidi <meghdad.1987@gmail.com>
2018-07-10 07:28:58 +0430

    Fixed: MongoDB error handled

--- server/index.js
+++ server/index.js
@@ -2,13 +2,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
+// Utils
+import chalk from 'chalk';
+
 // Routes
 import routes from './routes';
 
 // Database connection
 mongoose.connect('mongodb://localhost/crm');
 const db = mongoose.connection;
-db.on('error', () => console.error('Mongo connection error!'));
+db.on('error', (err) => console.log(chalk.red('Mongo connection error: '+ err.message)));
 db.once('open', () => {
     console.log('Connected to MongoDb');
 });
